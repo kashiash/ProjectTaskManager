648030758d3da46eda3a4613d027a063707d4937
Jacek Kosinski <kashiash@gmail.com>
2023-09-01 21:21:35 +0200

    Initial Commit

---
+++ ProjectTaskManager/ProjectTaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTaskManagerApp.swift
+//  ProjectTaskManager
+//
+//  Created by Jacek Kosinski U on 01/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjectTaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
