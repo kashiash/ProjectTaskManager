0b4e2c0e8d4b8a3abadab21042fba48eabef1ef3
Jacek Kosinski <kashiash@gmail.com>
2023-09-02 10:52:35 +0200

    modele

---
+++ ProjectTaskManager/Model/Task.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+
+struct Task: Identifiable {
+    internal init(title: String, isCompleted: Bool = false, dueDate: Date = Date(), details: String? = nil) {
+        self.title = title
+        self.isCompleted = isCompleted
+        self.dueDate = dueDate
+        self.details = details
+    }
+
+
+    let id = UUID()
+    var title: String
+    var isCompleted: Bool
+    var dueDate: Date
+    var details: String?
+
+
+    static func example() -> Task {
+        Task(title: "Kup flaszkę",  dueDate: Calendar.current.date(byAdding: .day, value: 2, to: Date())!, details: "Najchętniej Taliskera")
+    }
+
+    static func examples() -> [Task] {
+        return [
+            Task(title: "Kup chleb", details: "A jak będą to jajka, to tuzin"),
+            Task(title: "Przygotuj prezentację", dueDate: Calendar.current.date(byAdding: .day, value: 3, to: Date())!, details: "Dla spotkania zespołu"),
+            Task(title: "Odwiedź lekarza", dueDate: Calendar.current.date(byAdding: .day, value: 7, to: Date())!, details: "Badanie ogólne"),
+            Task(title: "Zaplanuj wakacje", dueDate: Calendar.current.date(byAdding: .month, value: 2, to: Date())!, details: "Trasa i zakwaterowanie"),
+            Task(title: "Zrób zakupy spożywcze"),
+            Task(title: "Napisz list motywacyjny", dueDate: Calendar.current.date(byAdding: .day, value: 5, to: Date())!, details: "Podanie o pracę"),
+            Task(title: "Uczyć się nowego języka programowania", details: "Najlepiej PHP, przyda sie w robocie"),
+            Task(title: "Obejrzyj ulubiony film", details: "Wieczór filmowy z przyjaciółmi"),
+            Task(title: "Przeczytaj książkę", details: "Dokładnie 50 stron dziennie"),
+            Task(title: "Zorganizuj urodziny", dueDate: Calendar.current.date(byAdding: .month, value: 1, to: Date())!, details: "Lista gości i menu"),
+            Task(title: "Napraw wyciek kranu"),
+            Task(title: "Rozpocznij kurs online", dueDate: Calendar.current.date(byAdding: .day, value: 10, to: Date())!, details: "Programowanie w Swift"),
+            Task(title: "Ogarnij dokumenty podatkowe", dueDate: Calendar.current.date(byAdding: .month, value: 3, to: Date())!, details: "Dla księgowego"),
+            Task(title: "Kup prezent dla mamy", dueDate: Calendar.current.date(byAdding: .day, value: 14, to: Date())!, details: "Na urodziny"),
+            Task(title: "Zbuduj nową stronę internetową", details: "Projekt portfolio"),
+            Task(title: "Obejrzyj kurs jogi online"),
+            Task(title: "Zacznij bieganie", details: "3 razy w tygodniu"),
+            Task(title: "Zorganizuj piknik", dueDate: Calendar.current.date(byAdding: .month, value: 2, to: Date())!, details: "Na świeżym powietrzu"),
+            Task(title: "Przetestuj nową aplikację", dueDate: Calendar.current.date(byAdding: .day, value: 2, to: Date())!, details: "Feedback dla deweloperów")
+        ]
+    }
+
+}
+
---
+++ ProjectTaskManager/Model/TaskGroup.swift
@@ -0,0 +1,53 @@
+//
+//  TaskGroup.swift
+//  ProjectTaskManager
+//
+//  Created by Jacek Kosinski U on 02/09/2023.
+//
+
+import Foundation
+
+struct TaskGroup:Identifiable {
+    internal init(title: String,  tasks: [Task] = []) {
+        self.title = title
+        self.creationDate = Date()
+        self.tasks = tasks
+    }
+
+    let id = UUID()
+    var title:String
+    let creationDate: Date
+    var tasks: [Task]
+
+    static func example() ->  TaskGroup {
+
+        let task1 =  Task(title: "Kup wino czerwone", details: "Ulubione wino do kolacji")
+        let task2 =  Task(title: "Zakup whisky", details: "Single malt Scotch")
+        let task3 =  Task(title: "Kup piwo", details: "Craft beer z lokalnej browarni, mocno chmielone!")
+
+        var group = TaskGroup(title: "Zakupy pierwszej potrzeby")
+        group.tasks = [task1,task2,task3]
+        return group
+    }
+
+    static func examples() -> [TaskGroup] {
+        let group1 = TaskGroup.example()
+        let group2 = TaskGroup(title: "Zakupy opcjonalne",  tasks: [
+            Task(title: "Kup pieluchy", details: "Rozmiar 2, paczka 100 sztuk"),
+            Task(title: "Zakup mleko modyfikowane", details: "Dla niemowląt"),
+        ])
+        let group3 = TaskGroup(title: "Projekt domowy",  tasks: [
+            Task(title: "Zaprojektuj kuchnię", details: "Nowa zabudowa meblowa"),
+            Task(title: "Kup materiały budowlane", details: "Farba, płytki, narzędzia"),
+        ])
+        let group4 = TaskGroup(title: "Programowanie",  tasks: [
+            Task(title: "Napisz kod źródłowy", details: "Implementacja nowego funkcjonalności"),
+            Task(title: "Zaktualizuj dokumentację", details: "Opis nowych zmian"),
+            Task(title: "Testowanie oprogramowania", details: "Sprawdzenie poprawności działania"),
+            Task(title: "Rozwiązywanie błędów", details: "Debugowanie aplikacji"),
+            Task(title: "Spotkanie z zespołem", details: "Omówienie postępu prac"),
+        ])
+        return [group1, group2, group3, group4]
+    }
+
+}
